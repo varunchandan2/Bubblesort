a458b6a9a0208bc734d12b080391d71903a6e154
Varun <varunchandan1999@gmail.com>
2021-07-20 08:29:47 -0700

    Worked on 2nd function of Palindrome check

--- String/PalindromeCheck.py
+++ String/PalindromeCheck.py
@@ -4,10 +4,22 @@ string = 'abcdcba'
 # Solving iteratively by creating a new string and checking of both string matches
 # Time complexity is O(n^2) | Space comlexity is O(n)
 
-def palindromeCheck(string):
+def palindromeCheck1(string):
     reversedString = ""
     for i in reversed(range(len(string))):
         reversedString += string[i]
     return string == reversedString
 
-print(palindromeCheck(string))
+print(palindromeCheck1(string))
+
+
+# Solving iteratively with array which improves the time complexity 
+# Time complexity is O(n) | Space comlexity is O(n)
+
+def palindromeCheck2(string):
+    reversedChars = []
+    for i in reversed(range(len(string))):
+        reversedChars.append(string[i])
+    return string == "".join(reversedChars)
+
+print(palindromeCheck2(string))
